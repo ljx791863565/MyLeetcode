d2b30777adf135249d6900e715ef5f295901401e
nick138797 <nick-tech@xyunstech.com>
2018-05-25 17:49:18 +0800

    18.5.25 at company

--- algorithm/sort/sort.c
+++ algorithm/sort/sort.c
@@ -45,13 +45,13 @@ void sort3(int *arr, int n)
 	int max = arr[0];
 	for (int i=1; i<n; ++i){
 		if (arr[i] > max){
-			max = arr[i];
+			max = arr[i];	//找出需要最大的空间
 		}
 	}
 	printf("max = %d\n", max);
-	int *p = (int*)malloc(max+1);
+	int *p = (int*)malloc((max+1)*(sizeof(int)));	//申请max+1 个 int大小的内存空间
 	for (int i=0; i<max+1; ++i){
-		p[i] = 0;		
+		p[i] = 0;	//清空		
 	}
 	for (int i=0; i<n; ++i){
 		p[arr[i]]++;//计算每个元素出现的次数
@@ -65,6 +65,55 @@ void sort3(int *arr, int n)
 
 	free(p);
 }
+
+//快速排序
+//一般来说非常快
+//时间复杂度 O(nlog2n)
+//low 数组下限		high 数组上限
+void sort4(int *arr, int low, int high)
+{
+	int mid = arr[low];
+	while (low < high){
+		while (low < high && arr[high] >= mid){
+			high--;
+		}
+		arr[low] = arr[high];	//比中轴小的移动到左端
+
+		while (low < high && arr[low] < mid){
+			low++;
+		}
+		arr[high] = arr[low];	//比中轴大的移动到右端
+	}
+	arr[low] = mid;
+	return ;
+}
+
+/*
+//递归的快速排序
+void sort4_1(int *arr, int low, int high)
+{
+	while (low < high){
+		int mid = sort4(arr, low, high)
+	}
+}
+*/
+
+//插入排序
+//时间复杂度 O(n^2)
+void sort5(int *arr, int n)
+{
+	int tmp = 0;
+	int i, j;
+	for (i=0; i<n; i++){
+		tmp = arr[i];
+		//假如tmp比前面的值小，将tmp前面的值后移
+		for (j=i; j>0 && tmp < arr[j-1]; j--){
+			arr[j] = arr[j-1];
+		}
+		arr[j] = tmp;
+	}
+}
+
 int main()
 {
 	int n, i;
@@ -72,13 +121,14 @@ int main()
 	scanf("%d", &n);
 	int arr[n];
 	srand((unsigned)time(NULL));
-	for (i=0; i<n; ++i){
+	for (i=0; i<n; i++){
 		arr[i] = rand()%100000 +1;
 	}
 	show(arr,n);
 	printf("after sotr: \n");
 	start = clock();
-	sort3(arr, n);
+	sort5(arr, n);
+//	sort4(arr, 0, n);
 	stop = clock();
 	show(arr,n);
 	duration = ((double)(stop - start)) /CLOCKS_PER_SEC;
