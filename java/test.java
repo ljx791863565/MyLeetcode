73b2a083d24eee34f179c7f021d8e65b33b34acc
JiaXing Liao <liaojia138797@163.com>
2018-06-03 10:18:54 +0800

    16.6.3 at home

---
+++ java/test.java
@@ -0,0 +1,12 @@
+public class test{
+	public static void main(String[] args){
+		String s = new String("sbcd");//new 一个String对象
+		System.out.println(s);			//打印s
+		char c = 'x';
+		System.out.println(c);
+		Character ch = new Character(c);
+		//Character ch = new Character('a');	//也可以这么用
+		System.out.println(ch);
+
+	}
+}
