1c3b38156eb8b74d237169273fe827c5ed719edf
nick <nick-tech@xyunstech.com>
2018-07-06 14:06:00 +0800

    java

--- java/Test.java
+++ java/Test.java
@@ -3,5 +3,9 @@
 public class Test{
 	public static void main(String [] argv){
 		System.out.println("hello world!");
+		int [] array = new int [] {4, 5, 6, 7};
+		for (int i=0; i<4; i++){
+			System.out.println(array[i]);
+		}
 	}
 }
